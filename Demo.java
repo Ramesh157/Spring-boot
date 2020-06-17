dfc8845ab132e8f312d672958617779ba7fed543
Ramesh157 <j.ramesh157@gmail.com>
2020-06-17 21:15:13 +0530

    m2 method commented

--- Demo.java
+++ Demo.java
@@ -2,7 +2,7 @@ public class Demo{
 
 
  public void m1(){}
- public void m2() {}
+ public void m2() {}//write logic here
    public static void main(String []args){
 	   //logic goes here
 	 Demo dm= new Demo();
