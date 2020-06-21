0b047e02094b48ed764345c76838936ce9512f42
Ramesh157 <j.ramesh157@gmail.com>
2020-06-21 10:15:24 +0530

    duplicate main method deleted

--- Demo.java
+++ Demo.java
@@ -4,11 +4,11 @@ public class Demo{
  public void m1(){
  System.out.println("M1() method calling};
  }
-   public static void main(String []args){
+   
 	   //logic goes here
 	 Demo dm= new Demo();
 	 
 	 dm.m1();
 	 
-     }
+     
  }
