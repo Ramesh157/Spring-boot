8c423ff1e683fbbc261a5f6aef28965c564e08df
Ramesh157 <j.ramesh157@gmail.com>
2020-06-21 10:12:44 +0530

    m1() method called

--- Demo.java
+++ Demo.java
@@ -1,10 +1,14 @@
 public class Demo{
 
 
- public void m1();
+ public void m1(){
+ System.out.println("M1() method calling};
+ }
    public static void main(String []args){
 	   //logic goes here
 	 Demo dm= new Demo();
+	 
+	 dm.m1();
 	 
      }
  }
